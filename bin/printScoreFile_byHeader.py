2e565c4422777a7f8849838c3063dd4589b8846d
jvarga <julia.varga@cs.huji.ac.il>
2022-02-20 14:53:14 +0200

    debugged workflow

--- bin/printScoreFile_byHeader.py
+++ bin/printScoreFile_byHeader.py
@@ -9,7 +9,7 @@ def main():
     score_file = sys.argv[1]
     scores = sys.argv[2:]
 
-    score_df = pd.read_csv(score_file, skiprows=0, header=1, sep=r"\s+", low_memory=False)
+    score_df = pd.read_csv(score_file, skiprows=0, header=0, sep=r"\s+", low_memory=False)
     score_df['src_file'] = os.path.split(score_file)[1] # not really needed only for compatibility with old code
     score_df = score_df.query('total_score!="total_score"') # sometimes score name lines are repeated, we need to get rid of them
     output = score_df[['src_file'] + scores]
