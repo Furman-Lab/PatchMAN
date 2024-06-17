89ead72d899e99a0471abd8e35de2e9037784b2f
jvarga <julia.varga@cs.huji.ac.il>
2024-06-17 15:38:43 +0300

    ls

--- bin/run_extract_templates.sh
+++ bin/run_extract_templates.sh
@@ -3,13 +3,13 @@
 #SBATCH --time=93:00:00
 #SBATCH --mem=1600m
 #SBATCH --nice=8000
-#SBATCH --module="singularity"
+#SBATCH --get-user-env
 
 #$1: peptide sequence
 #$2: receptor pdb
 #$3: other arguments passed by main script
 
-source $PROTOCOL_ROOT/.env ",/$(realpath "$work_dir" | cut -d'/' -f2)"
+#source $PROTOCOL_ROOT/.env ",/$(realpath "$work_dir" | cut -d'/' -f2)"
 
 matches=($(ls *matches))
 
