9793824d5c352285ce2fd1cb1e9d8bc5c9bbaeaf
jvarga <julia.varga@cs.huji.ac.il>
2025-01-20 14:44:17 +0200

    removed unnecessary files

--- compile_singularity_at_huji.sh
+++
@@ -1,2 +0,0 @@
-rundeb11 -root -user -bind $PWD -run "cd $PWD/containers; singularity build rosetta.sif rosetta.def; singularity build master.sif master.def; singularity build pyrosetta.def pyrosetta.sif"
-
