e3a899294c62e3d5bc0640d28006a2ca82695f37
jvarga <julia.varga@cs.huji.ac.il>
2025-01-22 22:12:54 +0200

    removed unnecessary tracked files

--- bin/.ipynb_checkpoints/cluster-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --time=24:00:00
-#SBATCH --mem=2000m
-#SBATCH --get-user-env
-
-python3 "$PROTOCOL_ROOT/bin/cluster.py"
--- bin/.ipynb_checkpoints/prepare_input-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=prepack_receptor
-#SBATCH --time=90:00:00
-#SBATCH --mem=1600m
-#SBATCH --get-user-env
-
-python3 ${PROTOCOL_ROOT}/bin/prepack_receptor.py $1
