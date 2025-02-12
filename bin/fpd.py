063fd2dbdd5a47e657bd060a569841ebf31894b2
jvarga <julia.varga@cs.huji.ac.il>
2025-02-12 14:56:33 +0200

    added possibility to restart run after fail, without rerunning from scratch

--- bin/fpd.py
+++ bin/fpd.py
@@ -34,8 +34,10 @@ def parse_mem_string(mem_string, multiply_by):
     mem_int = int(''.join(filter(str.isdigit, mem_string)))
     print(mem_int)
     mem_unit = ''.join(filter(str.isalpha, mem_string))
+    print(mem_unit)
 
     return str(mem_int * multiply_by) + mem_unit
+
 
 def protocol(pose_path, min_rec_bb, index, output_path):
     """Run FlexPepDock protocol and write to worker-specific silent files."""
@@ -89,7 +91,7 @@ def protocol(pose_path, min_rec_bb, index, output_path):
     sys.stdout.flush()
 
 
-def run_fpd_cluster(list_of_inputs, init_opts, min_rec_bb=False, output_path='.', nstruct=1):
+def run_fpd_cluster(list_of_inputs, init_opts, min_rec_bb=False, output_path='.', nstruct=1, finished_decoys=None):
     from dask.distributed import Client
     from dask_jobqueue import SLURMCluster
     import socket
@@ -99,9 +101,34 @@ def run_fpd_cluster(list_of_inputs, init_opts, min_rec_bb=False, output_path='.'
     pyrosetta.distributed.init(init_opts)
 
     # Setup the cluster
+    if 'FPD_NUM_JOBS' not in os.environ:
+        os.environ['FPD_TASKS_PER_JOB'] = str(os.cpu_count() // 2)
     tasks_per_job = os.environ['FPD_TASKS_PER_JOB']                                     # Number of tasks per node (one per core)
-    memory_per_job = parse_mem_string(os.environ['FPD_MEM_PER_TASK'], tasks_per_job)    # Memory per task
+    
+    if 'FPD_MEM_PER_TASK' not in os.environ:
+        memory_per_job = f'{tasks_per_job * 2}G'
+    else:
+        memory_per_job = parse_mem_string(os.environ['FPD_MEM_PER_TASK'], tasks_per_job)    # Memory per task
 
+    if 'FPD_TIME' not in os.environ:
+        os.environ['FPD_TIME'] = "24:00:00"
+        
+    if 'FPD_NUM_JOBS' not in os.environ:
+        os.environ['FPD_NUM_JOBS'] = str(len(list_of_inputs) * nstruct)
+        
+    # Run tasks
+    if finished_decoys is None:
+        task_args = [(pose_path, min_rec_bb, i, output_path)
+                 for pose_path in list_of_inputs for i in range(1, nstruct + 1)]
+    else:
+        task_args = []
+        for pose_path in list_of_inputs:
+            basename = os.path.splitext(os.path.basename(pose_path))[0]
+            for i in range(1, nstruct + 1):
+                if f"{basename}_{i:04d}" not in finished_decoys:
+                    task_args.append((pose_path, min_rec_bb, i, output_path))
+        print(f"Resuming with {len(task_args)} decoys to run out of {len(list_of_inputs) * nstruct} decoys")
+        
     # Dashboard will be available at ip of localhost, port: 8787
     print(f"Dashboard URL: http://{socket.gethostname()}:8787")
     print(f"Submitting job array with {tasks_per_job} tasks per job, {memory_per_job} memory per job")
@@ -123,10 +150,6 @@ def run_fpd_cluster(list_of_inputs, init_opts, min_rec_bb=False, output_path='.'
     )
     cluster.scale(jobs=1)
     client = Client(cluster)
-
-    # Run tasks
-    task_args = [(pose_path, min_rec_bb, i, output_path)
-                 for pose_path in list_of_inputs for i in range(1, nstruct + 1)]
 
     futures = client.map(lambda args: protocol(*args), task_args)
     client.gather(futures)
@@ -168,9 +191,14 @@ def set_nstruct(n_templates, max_nstruct_per_decoy, max_nstruct_total=20000, arg
 
 
 # Clean up existing output files before running FlexPepDock
-def clean_before_run():
+def clean_before_run(force_rerun):
     for file in ["score.sc", "decoys.silent"]:
         Path(file).unlink(missing_ok=True)
+        
+    if force_rerun:
+        print("Deleting half-finished jobs...")
+        for file in glob.glob("decoys_worker_*.silent"):
+            Path(file).unlink()
 
 
 # process redundancy and/or benchmarking
@@ -201,6 +229,18 @@ def create_init_opts(args):
     return init_opts
 
 
+def list_decoys_for_restarting():
+    # We need the list of decoys that were already run to restart the process
+    finished_decoys = []
+    for file in glob.glob("decoys_worker_*.silent"):
+        with open(file, "r") as infile:
+            for line in infile:
+                if line.startswith("SCORE") and "description" not in line:
+                    finished_decoys.append(line.split()[-1])
+    
+    return finished_decoys
+
+
 def main():
     parser = argparse.ArgumentParser(description="Controls the FlexPepDock refinement part of the protocol.")
     parser.add_argument("-t", "--nstruct", type=int, help="Number of FlexPepDock decoys to generate. Default: 1")
@@ -208,6 +248,7 @@ def main():
     parser.add_argument("-u", "--unboundrot", type=str, help="Add unbound rotamers for the receptor. Default: None")
     parser.add_argument("-a", "--native", type=str, help="Native structure for comparison. Default: None")
     parser.add_argument("-b", "--benchmark", action="store_true", help="Enable benchmarking mode")
+    parser.add_argument("-f", "--force_rerun", action="store_true", help="Force rerunning jobs that stopped in the mid. Default: False")
     parser.add_argument("-r", "--redundancy", action="store_true", help="Filter for template redundancy. Default: False")
     parser.add_argument("-c", "--cpu", type=int, help="Number of CPUs to use. Default: All available")
     parser.add_argument("-o", "--outdir", type=str, help="Output directory. Default: Current directory", default=os.getcwd())
@@ -226,15 +267,18 @@ def main():
     init_opts = create_init_opts(args)
 
     # Clean up existing output files before running FlexPepDock
-    clean_before_run()
-
+    clean_before_run(args.force_rerun)
+    
+    # If we did not want to force rerunning and there are some decoys already finished, we need to list them
+    finished_decoys = list_decoys_for_restarting() if not args.force_rerun else None
+    
     # Open dask client and link to dashboard
     from dask.config import set
     set({'distributed.worker.memory.target': 0.8, 'distributed.worker.memory.spill': 0.75, 'distributed.worker.memory.pause':0.85})
 
     # Run FlexPepDock
     print('Running FlexPepDock...')
-    run_fpd_cluster(input_files, init_opts, args.min_rec_bb, args.outdir, int(nstruct))
+    run_fpd_cluster(input_files, init_opts, args.min_rec_bb, args.outdir, int(nstruct), finished_decoys)
 
 
 if __name__ == "__main__":
