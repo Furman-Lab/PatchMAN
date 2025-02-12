c2702fa1c41116f4f565cca9107b98206f497455
jvarga <julia.varga@cs.huji.ac.il>
2025-02-12 14:57:15 +0200

    moved some functions to script, for easier integeration to ROSIE

--- bin/split_to_motifs.py
+++ bin/split_to_motifs.py
@@ -5,6 +5,7 @@ import os
 import sys
 import argparse
 import pyrosetta_utils as utils
+import protocol_utils
 from types import SimpleNamespace
 
 NEIGHBORS_DIST = 10
@@ -332,6 +333,12 @@ def split_to_motifs():
 	# split the surface into motifs
 	motifs = define_motifs(pose, prot_name, check_list=check_list, focus=args.focus, hotspot_mode=args.hotspot_mode, debug=args.verbose)
 	print("The surface was split into " + str(len(set(motifs))) + " patches")
+	
+	# Listing motif files and creating motif_list
+	protocol_utils.create_motif_list(os.path.basename(args.input)[:-4], 'motif_list')
+	
+	# Create search files for MASTER
+	protocol_utils.run_createPDS('motif_list')
 
 
 if __name__ == "__main__":
