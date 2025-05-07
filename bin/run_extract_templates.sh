29334cccfb736f1f02ce64aa5aa5a60d171b3587
jvarga <julia.varga@cs.huji.ac.il>
2025-05-07 11:43:15 +0300

    not using outside libraries

--- bin/run_extract_templates.sh
+++ bin/run_extract_templates.sh
@@ -19,4 +19,5 @@ motif=${motifs["$SLURM_ARRAY_TASK_ID"]}
 
 echo $1 > pepfile
 
+export PYTHONNOUSERSITE=1
 python3 ${BIN_DIR}/extract_peps_for_motif.py -m "$match_list" -p pepfile -r "$2" --patch "$motif" "${@:3}"
