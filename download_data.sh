e9511ecafe4b6b618d43c14cbe5c8eb78baf99bf
jvarga <julia.varga@cs.huji.ac.il>
2025-07-24 11:16:51 +0300

    making download more robust

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -eu
+set -o pipefail
 
 ###########################################################
 # Download and extract the compiled database for running  #
@@ -8,26 +10,29 @@
 ###########################################################
 
 DB_DIR="databases/"
-mkdir -p $DB_DIR
+mkdir -p "$DB_DIR"
 
 ############# DATABASE #############
 # Download database for Master
 echo "Downloading MASTER database. This may take a while..."
 MASTERDB="$DB_DIR/masterDB/"
 mkdir -p $MASTERDB
-curl -o "$DB_DIR/masterDB.tar.gz" -L "https://www.cs.huji.ac.il/~jvarga/master_search.tar.gz"
+#curl -o "$DB_DIR/masterDB.tar.gz" -L "https://www.cs.huji.ac.il/~jvarga/master_search.tar.gz"
+curl -o "$DB_DIR/masterDB.tar.gz" -C - -L --retry 10 --retry-delay 5 --retry-max-time 0 --max-time 0 "https://www.cs.huji.ac.il/~jvarga/master_search.tar.gz"
 
 echo "Extracting MASTER search database..."
 tar -xzf "$DB_DIR/masterDB.tar.gz" -C $DB_DIR
 find "$(realpath "$DB_DIR/masterDB/")" -name '*pds' > db_list_30nonred # Create list file with full paths
+rm "$DB_DIR/masterDB.tar.gz" -f
 
 # Download cleaned PDB files for Master
 echo "Downloading and extracting cleaned PDBs for MASTER search. This may take a while..."
-curl -o "$DB_DIR/master_clean.tar.gz" -L "https://www.cs.huji.ac.il/~jvarga/master_extract.tar.gz"
+#curl -o "$DB_DIR/master_clean.tar.gz" -L "https://www.cs.huji.ac.il/~jvarga/master_extract.tar.gz"
+curl -o "$DB_DIR/master_clean.tar.gz" -C - -L --retry 10 --retry-delay 5 --retry-max-time 0 --max-time 0 "https://www.cs.huji.ac.il/~jvarga/master_extract.tar.gz"
 
 echo "Extracting cleaned PDBs..."
 tar -xzf "$DB_DIR/master_clean.tar.gz" -C "$DB_DIR"
 
-rm "$DB_DIR/master_clean.tar.gz" "$DB_DIR/masterDB.tar.gz"
+rm "$DB_DIR/master_clean.tar.gz"
 
 echo "Downloads are finished!"
